672d488d141e2c6b76635337852dbdd935a0c102
Conner Krenzer <krenzer84@gmail.com>
2025-01-17 08:56:23 -0500

    resource_assignmnet_migration=finished

--- data/migrations/20250116233722_first-migration.js
+++ data/migrations/20250116233722_first-migration.js
@@ -28,6 +28,25 @@ exports.up = async function (knex) {
         .inTable("projects")
         .onDelete("RESTRICT")
         .onUpdate("RESTRICT");
+    })
+    .createTable("project_resources", (table) => {
+      table.increments("project_resource_id");
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      table
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resource_id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
     });
 };
 
